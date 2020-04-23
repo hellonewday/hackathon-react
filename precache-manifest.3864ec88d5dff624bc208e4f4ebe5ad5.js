e539c03cd63bc417807c8b8eff2b9030fe1f600e
Maximuss Hùng <vidostartup@gmail.com>
2020-04-23 15:37:34 +0700

    Updates

---
+++ precache-manifest.3864ec88d5dff624bc208e4f4ebe5ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d7a3fd69c2d2ca963f4818a0a83471",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "941c20ecc47d30a7348b",
+    "url": "/covid19/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "71ce42e948d2b1497b6a",
+    "url": "/covid19/static/js/2.7a05b326.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19/static/js/2.7a05b326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941c20ecc47d30a7348b",
+    "url": "/covid19/static/js/main.eaf79948.chunk.js"
+  },
+  {
+    "revision": "7559497984dd1cc6cff7",
+    "url": "/covid19/static/js/runtime-main.0cac853b.js"
+  }
+]);
